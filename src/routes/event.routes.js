589db0334bd61cc0254b3ab1747b332edbd94f7e
Victor Hernández <vhernandez@nabenik.dev>
2022-07-11 15:44:58 -0600

    first version

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,6 +1,5 @@
 import * as eventController from '../controllers/events.controller';
 import {Router} from 'express';
-import { route } from 'express/lib/application';
 const router = Router();
 
 router.get('/', eventController.getEvents);
